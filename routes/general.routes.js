ab554c09aebfb96c480f0bc97123196b21631a64
Alexander Minchev <aleksander.minchev18@gmail.com>
2022-05-04 14:52:09 +0200

    Update/Create Vehicle and Driver

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -5,18 +5,13 @@ const {execute} = require("../database/mysql.connector")
 const {Driver} = require("../model/driver.model");
 const {Vehicle} = require("../model/vehicle.model")
 router.post("/createDriver",
-    [
-    check("typeOfDriver","Type of Driver not provided").exists(),
-    check("firstName","First name not provided").exists(),
-    check("secondName","Second name not provided").exists(),
-    check("companyName","Company not provided").exists(),
-    check("email","Email not provided").exists(),
-    check("phone","Phone not provided").exists(),
-    check("address","Address not provided").exists(),
-    check("duns","Duns not provided").exists(),
-    check("zipcode","Zip code not provided").exists(),
-    check("city","City not provided").exists(),
-    ],async(req, res)=>{
+  [
+  check("firstName","First name not provided").exists(),
+  check("secondName","Second name not provided").exists(),
+  check("email","Email not provided").exists(),
+  check("phone","Phone not provided").exists(),
+  check("free","Free status not provided").exists(),
+],async(req, res)=>{
     try {
         const errors =validationResult(req);
         if (!errors.isEmpty()) {
@@ -25,10 +20,10 @@ router.post("/createDriver",
               message: "Invalid data while creating a Driver",
             });
          }
-         const {typeOfDriver,firstName,secondName,companyName,email,phone,address,duns,zipcode,city} = req.body;
-         const driver = new Driver(undefined,typeOfDriver,firstName,secondName,companyName,email,phone,address,duns,zipcode,city);
-         console.log(Driver)
-         const response= await Driver.createDriver(driver)
+        const {firstName,secondName,email,phone,free} = req.body;
+        const driver = new Driver(undefined,firstName,secondName,email,phone,free);
+        console.log(driver)
+        const response= await Driver.createDriver(driver)
         return res.status(200).json({response}) 
     } catch (error) {
         console.log(error);
@@ -44,17 +39,12 @@ router.post("/createDriver",
 
 
 router.post("/updateDriver",[
-    check("idCustomer","Id of customer not provided").exists(),
-    check("typeOfDriver","Type of Driver not provided").exists(),
+    check("idDriver","Id of driver not provided").exists(),
     check("firstName","First name not provided").exists(),
     check("secondName","Second name not provided").exists(),
-    check("companyName","Company not provided").exists(),
     check("email","Email not provided").exists(),
     check("phone","Phone not provided").exists(),
-    check("address","Address not provided").exists(),
-    check("duns","Duns not provided").exists(),
-    check("zipcode","Zip code not provided").exists(),
-    check("city","City not provided").exists(),
+    check("free","Free status not provided").exists(),
     ] ,async (req, res)=>{
         try {
             const errors =validationResult(req);
@@ -64,11 +54,11 @@ router.post("/updateDriver",[
                   message: "Invalid data while creating a Driver",
                 });
              }
-             const {idCustomer,typeOfDriver,firstName,secondName,companyName,email,phone,address,duns,zipcode,city} = req.body;
-             const driver = new Driver(idCustomer,typeOfDriver,firstName,secondName,companyName,email,phone,address,duns,zipcode,city);
-             console.log(Driver);
-             const response = await Driver.updateDriver(driver)
-             return res.status(200).json({response})
+            const {idDriver,firstName,secondName,email,phone,free} = req.body;
+            const driver = new Driver(idDriver,firstName,secondName,email,phone,free);
+            console.log(driver);
+            const response = await Driver.updateDriver(driver)
+            return res.status(200).json({response})
         } catch (error) {
             console.log(error);
             return res.status(500).json({
@@ -83,15 +73,9 @@ router.post("/updateDriver",[
 router.post("/createVehicle",
     [
     check("typeOfVehicle","Type of Vehicle not provided").exists(),
-    check("firstName","First name not provided").exists(),
-    check("secondName","Second name not provided").exists(),
-    check("companyName","Company not provided").exists(),
-    check("email","Email not provided").exists(),
-    check("phone","Phone not provided").exists(),
-    check("address","Address not provided").exists(),
-    check("duns","Duns not provided").exists(),
-    check("zipcode","Zip code not provided").exists(),
-    check("city","City not provided").exists(),
+    check("identifier","Identifier not provided").exists(),
+    check("storageSize","Storage size name not provided").exists(),
+    check("free","Free status not provided").exists(),
     ],async(req, res)=>{
     try {
         const errors =validationResult(req);
@@ -101,8 +85,8 @@ router.post("/createVehicle",
               message: "Invalid data while creating a Vehicle",
             });
          }
-         const {typeOfVehicle,firstName,secondName,companyName,email,phone,address,duns,zipcode,city} = req.body;
-         const vehicle = new Vehicle(undefined,typeOfVehicle,firstName,secondName,companyName,email,phone,address,duns,zipcode,city);
+         const {typeOfVehicle,identifier,storage_size,free} = req.body;
+         const vehicle = new Vehicle(undefined,typeOfVehicle,identifier,storage_size,free);
          console.log(vehicle)
          const response= await Vehicle.createVehicle(vehicle)
         return res.status(200).json({response}) 
@@ -117,17 +101,11 @@ router.post("/createVehicle",
       }
     })
 router.post("/updateVehicle",[
-    check("idCustomer","Id of customer not provided").exists(),
+    check("idVehicle","Id of vehicle not provided").exists(),
     check("typeOfVehicle","Type of Vehicle not provided").exists(),
-    check("firstName","First name not provided").exists(),
-    check("secondName","Second name not provided").exists(),
-    check("companyName","Company not provided").exists(),
-    check("email","Email not provided").exists(),
-    check("phone","Phone not provided").exists(),
-    check("address","Address not provided").exists(),
-    check("duns","Duns not provided").exists(),
-    check("zipcode","Zip code not provided").exists(),
-    check("city","City not provided").exists(),
+    check("identifier","Identifier not provided").exists(),
+    check("storageSize","Storage size name not provided").exists(),
+    check("free","Free status not provided").exists(),
     ] ,async (req, res)=>{
         try {
             const errors =validationResult(req);
@@ -137,8 +115,8 @@ router.post("/updateVehicle",[
                   message: "Invalid data while creating a Vehicle",
                 });
              }
-             const {idCustomer,typeOfVehicle,firstName,secondName,companyName,email,phone,address,duns,zipcode,city} = req.body;
-             const vehicle = new Vehicle(idCustomer,typeOfVehicle,firstName,secondName,companyName,email,phone,address,duns,zipcode,city);
+             const {idVehicle,typeOfVehicle,identifier,storage_size,free} = req.body;
+             const vehicle = new Vehicle(idVehicle,typeOfVehicle,identifier,storage_size,free);
              console.log(vehicle);
              const response = await Vehicle.updateVehicle(vehicle)
              return res.status(200).json({response})
