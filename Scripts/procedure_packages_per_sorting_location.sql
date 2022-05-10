043016f97dfc538f4ba2da8acbcf13bd0f990bfa
Alex <a.sandrovschii@gmail.com>
2022-05-10 19:16:17 +0200

    formatted sql script

--- Scripts/procedure_packages_per_sorting_location.sql
+++ Scripts/procedure_packages_per_sorting_location.sql
@@ -1,6 +1,10 @@
 DELIMITER $$
 CREATE PROCEDURE `packages_per_sorting_location`()
 BEGIN
-select end_location as "sorting location",count(end_location) as "packages"   from location l join routes r on idlocation=end_location where typeoflocation_idtypeoflocation=2 group by idlocation ;
+SELECT end_location as "sorting location", count(end_location) as "packages"   
+FROM location l 
+JOIN routes r ON idlocation=end_location 
+WHERE typeoflocation_idtypeoflocation=2 
+GROUP BY idlocation ;
 END$$
 DELIMITER ;
