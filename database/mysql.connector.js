3a704fb486148dbbcb0c354fcd7f406fa58cef5f
Alexander Minchev <aleksander.minchev18@gmail.com>
2022-05-11 20:36:02 +0200

    Transactions method

--- database/mysql.connector.js
+++ database/mysql.connector.js
@@ -5,6 +5,11 @@ require("dotenv").config();
 const { createPool, createConnection } = require("mysql");
 
 let pool
+/**
+ * An initialization method for a pool of connections to the database.
+ * Everytime a query is requested 1 connection is given for usage and then released
+ * upon query completion or failure.
+ */
 const init = () => {
   try {
     pool = createPool({
@@ -22,6 +27,91 @@ const init = () => {
     throw new Error("failed to initialized pool");
   }
 };
+/**
+ * An assistance function which executes 1 query to the database within a transaction if an error occurs it rolls back the `query`
+ * @param {connection} connection Connection received from a pool of requests 
+ * @param {String} query A string `query` for database execution
+ * @param { String[] | object} params An array of values which will we passed to the `query` and put in the positions of `?` which are in the `query` string.
+ * @returns The values from the database will be retrived and returned
+ */
+const individualQuery = (connection, query,params) => {
+  let returnValue;
+  connection.query(query,params,(err,results) => {
+    if (err) {          //Query Error (Rollback and release connection)
+      connection.rollback(function() {
+        connection.release();
+        //Failure
+      });
+    } else {
+      connection.commit((err)=> {
+        if (err) {
+          connection.rollback(() =>{
+            connection.release();
+            //Failure
+        });
+        } else {
+          returnValue=results;
+          connection.release();
+          //Success
+        }
+      })
+    }
+  })
+return returnValue;
+}
+  /**
+   * A function which is used for executing multiple queries within a transaction
+   * @param {String[]} queriesAn Array of mysql queries that will be executed within a transaction. These can contain `?`s
+   *                              which will be replaced with values in `params`.
+   * @param {Array[]} params An array of arrays that is the same length as `queries`.
+   * Each array in `params` should contain values to replace the `?`s in the corresponding query in `queries`. If a query has no `?`s, an empty array should be provided.
+   * @returns A Promise that is fulfilled with an array of the
+   *                              results of the passed in queries. The results in the
+   *                              returned array are at respective positions to the
+   *                              provided queries.
+   */
+const executeTransaction = (
+  queries = Array(String),
+  params = Array(Array),
+) => {
+  try {  
+    if (!pool){
+      throw new Error(
+        "Pool was not created. Ensure pool is created when running the app."
+      );
+    }
+    if (queries.length !== params.length) {
+      return Promise.reject(
+          'Number of provided queries did not match the number of provided query values arrays'
+      )
+    }
+    return new Promise((resolve, reject) => { 
+      const results=[];
+      pool.getConnection((err,connection)=>{  
+        connection.beginTransaction((error)=>{
+          if(error){
+            connection.rollback(()=>{
+              console.log(error);
+              console.log("ERROR HAS OCCURED WITHIN THE TRANSACTION");
+              connection.release();
+              reject(error)
+            })
+          }else{
+            queries.forEach((element,index) => {
+              results.push(individualQuery(connection, element,params[index]));
+            });
+            resolve(results);
+          }
+
+        })
+      })
+    });
+  } catch (error) {
+    console.error("[mysql.connector][execute][Error]: ", error);
+    throw new Error("failed to execute MySQL query");
+  }
+};
+
 /**
  * executes SQL queries in MySQL db
  *
@@ -55,4 +145,5 @@ const execute = (
 module.exports = {
   init,
   execute,
+  executeTransaction,
 }
