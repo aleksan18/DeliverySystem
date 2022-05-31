9f28bbacbf440866db1e007a2ebad9468812d5c5
Alex <a.sandrovschii@gmail.com>
2022-05-31 20:58:39 +0200

    added input validation

--- routes/deliveries.routes.js
+++ routes/deliveries.routes.js
@@ -5,103 +5,187 @@ const { Package } = require("../model/package.model");
 const { Payment } = require("../model/payment.model");
 const { check, validationResult } = require("express-validator")
 
-router.post("/addDelivery", async (req, res) => {
-    // console.log("req.body in /addDelivery ", req.body)
-    const {
-        packages_idpackages,
-        priority,
-        payment_idpayment,
-        international,
-        start_location,
-        end_location,
-        message,
-        estimated_date,
-        start_date,
-        end_date,
-        uid
-    } = req.body;
-    // console.log(await Delivery.updateDeliveries(1,true,1,true,1,1,"","2021-07-19T01:30:07.000Z","2021-07-19T01:30:07.000Z","2021-07-19T01:30:07.000Z","D332CD90-8A43"))
-    const newDelivery = new Delivery(
-        null,
-        packages_idpackages,
-        priority,
-        payment_idpayment,
-        international,
-        start_location,
-        end_location,
-        message,
-        new Date(estimated_date),
-        new Date(start_date),
-        ((end_date != null) ? new Date(end_date) : null),
-        uid
-    )
+router.post("/addDelivery",
+    [
+        check("packages_idpackages").exists({ checkFalsy: true }).withMessage("Package not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("priority").exists({ checkFalsy: true }).withMessage("Information about delivery priority wasn't provided").trim()
+            .toInt().isInt({ min: 0, max: 1 }).withMessage("Wrong value provided"),
+        check("payment_idpayment").exists({ checkFalsy: true }).withMessage("Payment not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("international").exists({ checkFalsy: true }).withMessage("Information if delivery is international wasn't provided").trim()
+            .toInt().isInt({ min: 0, max: 1 }).withMessage("Wrong value provided"),
+        check("start_location").exists({ checkFalsy: true }).withMessage("Start location not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("end_location").exists({ checkFalsy: true }).withMessage("End location not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("message").optional().exists().withMessage("Message not provided").trim()
+            .isLength({ max: 150 }).withMessage("Message should be no more than 150 characters long"),
+        check("start_date").exists({ checkFalsy: true }).withMessage("Start date not provided").trim()
+            .isDate({ format: "YYYY-MM-DD hh:mm:ss" }).withMessage("Date has wrong format (should be 'YYYY-MM-DD hh:mm:ss')")
+            .isAfter().withMessage("Date selected can't be today's date.")
+            .toDate().withMessage("Value provided is not a date")
 
-    // console.log("newDelivery inside addDelivery", newDelivery.toString())
-    const { deliveryCreated, createdDelivery } = await Delivery.createDelivery(newDelivery);
-    // example of what Delivery.createDelivery() should return 
-    // OkPacket {
-    //     fieldCount: 0,
-    //     affectedRows: 1,
-    //     insertId: 14,
-    //     serverStatus: 2,
-    //     warningCount: 0,
-    //     message: '',
-    //     protocol41: true,
-    //     changedRows: 0
-    //   }
-    // values can be accessed through response.insertId
-    // console.log("newDelivery:", newDelivery.toString());
+    ], async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Invalid data while creating a user",
+                });
+            }
+            // console.log("req.body in /addDelivery ", req.body)
+            const {
+                packages_idpackages,
+                priority,
+                payment_idpayment,
+                international,
+                start_location,
+                end_location,
+                message,
+                start_date
+            } = req.body;
+            // console.log(await Delivery.updateDeliveries(1,true,1,true,1,1,"","2021-07-19T01:30:07.000Z","2021-07-19T01:30:07.000Z","2021-07-19T01:30:07.000Z","D332CD90-8A43"))
+            const newDelivery = new Delivery(
+                null,
+                packages_idpackages,
+                priority,
+                payment_idpayment,
+                international,
+                start_location,
+                end_location,
+                message,
+                start_date,
+                null,
+                null
+            )
 
-    if (deliveryCreated) {
-        return res.status(200).json({ response: { createdDelivery } });
+            // console.log("newDelivery inside addDelivery", newDelivery.toString())
+            const { deliveryCreated, createdDelivery } = await Delivery.createDelivery(newDelivery);
+            // example of what Delivery.createDelivery() should return 
+            // OkPacket {
+            //     fieldCount: 0,
+            //     affectedRows: 1,
+            //     insertId: 14,
+            //     serverStatus: 2,
+            //     warningCount: 0,
+            //     message: '',
+            //     protocol41: true,
+            //     changedRows: 0
+            //   }
+            // values can be accessed through response.insertId
+            // console.log("newDelivery:", newDelivery.toString());
 
-    } else {
-        return res.status(500).json({ response: { message: "Internal Server Error" } });
-    }
-})
-router.post("/updateDelivery", async (req, res) => {
-    console.log("req.body in /updateDelivery ", req.body)
-    const {
-        iddeliveries,
-        packages_idpackages,
-        priority,
-        payment_idpayment,
-        international,
-        start_location,
-        end_location,
-        message,
-        estimated_date,
-        start_date,
-        end_date,
-        uid
-    } = req.body;
-    const delivery = new Delivery(
-        iddeliveries,
-        packages_idpackages,
-        priority,
-        payment_idpayment,
-        international,
-        start_location,
-        end_location,
-        message,
-        new Date(estimated_date),
-        new Date(start_date),
-        ((end_date != null) ? new Date(end_date) : null),
-        uid
-    )
-    console.log("delivery inside /updateDelivery", delivery.toString())
-    const { deliveryInfoIsSame, updatedDelivery } = await Delivery.updateDelivery(delivery);
-    console.log("updatedDelivery ", updatedDelivery);
-    console.log("deliveryInfoIsSame ", deliveryInfoIsSame);
+            if (deliveryCreated) {
+                return res.status(200).json({ response: { createdDelivery } });
 
-    if (!deliveryInfoIsSame && typeof updatedDelivery === 'object') {
-        return res.status(200).json({ response: updatedDelivery });
-    } else if (!deliveryInfoIsSame && updatedDelivery === undefined) {
-        return res.status(500).json({ response: { message: "Internal Server Error" } });
-    } else if (deliveryInfoIsSame) {
-        return res.status(400).json({ response: updatedDelivery, message: "Delivery was not updated, because the delivery info is the same" });
-    }
-})
+            } else {
+                return res.status(500).json({ response: { message: "Internal Server Error" } });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: "Invalid data",
+                errors: [
+                    { value: error, msg: error.message },
+                ],
+            });
+        }
+    })
+
+router.post("/updateDelivery",
+    [
+        check("iddeliveries").exists({ checkFalsy: true }).withMessage("Delivery not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("packages_idpackages").exists({ checkFalsy: true }).withMessage("Package not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("priority").exists({ checkFalsy: true }).withMessage("Information about delivery priority wasn't provided").trim()
+            .toInt().isInt({ min: 0, max: 1 }).withMessage("Wrong value provided"),
+        check("payment_idpayment").exists({ checkFalsy: true }).withMessage("Payment not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("international").exists({ checkFalsy: true }).withMessage("Information if delivery is international wasn't provided").trim()
+            .toInt().isInt({ min: 0, max: 1 }).withMessage("Wrong value provided"),
+        check("start_location").exists({ checkFalsy: true }).withMessage("Start location not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("end_location").exists({ checkFalsy: true }).withMessage("End location not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("message").optional().exists().withMessage("Message not provided").trim()
+            .isLength({ max: 150 }).withMessage("Message should be no more than 150 characters long"),
+        check("start_date").exists({ checkFalsy: true }).withMessage("Start date not provided").trim()
+            .isDate({ format: "YYYY-MM-DD hh:mm:ss" }).withMessage("Date has wrong format (should be 'YYYY-MM-DD hh:mm:ss')")
+            .isAfter().withMessage("Date selected can't be earlier than today's date.")
+            .toDate().withMessage("Value provided is not a date"),
+        check("estimated_date").exists({ checkFalsy: true }).withMessage("Estimated date not provided").trim()
+            .isDate({ format: "YYYY-MM-DD hh:mm:ss" }).withMessage("Date has wrong format (should be 'YYYY-MM-DD hh:mm:ss')")
+            .isAfter().withMessage("Date selected can't be earlier than today's date.")
+            .toDate().withMessage("Value provided is not a date"),
+        check("end_date").optional().exists({ checkFalsy: true }).withMessage("End date not provided").trim()
+            .isDate({ format: "YYYY-MM-DD hh:mm:ss" }).withMessage("Date has wrong format (should be 'YYYY-MM-DD hh:mm:ss')")
+            .isAfter().withMessage("Date selected can't be  earlier than today's date.")
+            .toDate().withMessage("Value provided is not a date"),
+        check("uid").optional().exists().withMessage("UUID not provided").trim()
+            .isLength({ max: 20 }).withMessage("Message should be no more than 20 characters long"), // MUST BE CHANGED
+    ], async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Invalid data while creating a user",
+                });
+            }
+            console.log("req.body in /updateDelivery ", req.body)
+            const {
+                iddeliveries,
+                packages_idpackages,
+                priority,
+                payment_idpayment,
+                international,
+                start_location,
+                end_location,
+                message,
+                estimated_date,
+                start_date,
+                end_date,
+                uid
+            } = req.body;
+            const delivery = new Delivery(
+                iddeliveries,
+                packages_idpackages,
+                priority,
+                payment_idpayment,
+                international,
+                start_location,
+                end_location,
+                message,
+                estimated_date,
+                start_date,
+                ((end_date != null) ? new Date(end_date) : null),
+                uid
+            )
+            console.log("delivery inside /updateDelivery", delivery.toString())
+            const { deliveryInfoIsSame, updatedDelivery } = await Delivery.updateDelivery(delivery);
+            console.log("updatedDelivery ", updatedDelivery);
+            console.log("deliveryInfoIsSame ", deliveryInfoIsSame);
+
+            if (!deliveryInfoIsSame && typeof updatedDelivery === 'object') {
+                return res.status(200).json({ response: updatedDelivery });
+            } else if (!deliveryInfoIsSame && updatedDelivery === undefined) {
+                return res.status(500).json({ response: { message: "Internal Server Error" } });
+            } else if (deliveryInfoIsSame) {
+                return res.status(400).json({ response: updatedDelivery, message: "Delivery was not updated, because the delivery info is the same" });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: "Invalid data",
+                errors: [
+                    { value: error, msg: error.message },
+                ],
+            });
+        }
+    })
 
 
 // router.post("/addWholeDelivery", async (req, res) => {
@@ -243,3 +327,6 @@ router.post("/updateDelivery", async (req, res) => {
 // })
 
 module.exports = router;
+
+
+// select get_estimated_date(1,15,'2021-07-19 03:30:07') as estimated_date
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,16 +5,32 @@ const { execute } = require("../database/mysql.connector")
 const { User } = require("../model/user.model");
 router.post("/createUser",
     [
-        check("typeOfUser", "Type of user not provided").exists(),
-        check("firstName", "First name not provided").exists(),
-        check("secondName", "Second name not provided").exists(),
-        check("companyName", "Company not provided").exists(),
-        check("email", "Email not provided").exists(),
-        check("phone", "Phone not provided").exists(),
-        check("address", "Address not provided").exists(),
-        check("duns", "Duns not provided").exists(),
-        check("zipcode", "Zip code not provided").exists(),
-        check("city", "City not provided").exists(),
+        check("typeOfUser").exists({ checkFalsy: true }).withMessage("Type of user not provided").trim().toInt()
+            .isInt({ min: 1, max: 2 }).withMessage("Wrong value provided"),
+        check("firstName").exists({ checkFalsy: true }).withMessage("First name not provided").trim()
+            .isAlpha().withMessage("First name should contain only characters")
+            .isLength({ max: 45 }).withMessage("First name should be no more than 45 characters long"),
+        check("secondName").exists({ checkFalsy: true }).withMessage("Second name not provided").trim()
+            .isAlpha().withMessage("Second name should contain only characters")
+            .isLength({ max: 45 }).withMessage("Second name should be no more than 45 characters long"),
+        check("companyName").exists().withMessage("Company not provided").trim()
+            .isAlphanumeric().withMessage("Company name can have only characters and/or numbers provided")
+            .isLength({ max: 45 }).withMessage("Company name should be no more than 45 characters long"),
+        check("email").exists({ checkFalsy: true }).withMessage("Email not provided").trim()
+            .normalizeEmail().isEmail().withMessage("Wrong email format")
+            .isLength({ max: 45 }).withMessage("Email should be no more than 45 characters long"),
+        check("phone").exists({ checkFalsy: true }).withMessage("Phone not provided").trim()
+            .isMobilePhone().withMessage("Wrong phone format")
+            .isLength({ max: 16 }).withMessage("Phone should be no more than 16 characters long"),
+        check("address").exists({ checkFalsy: true }).withMessage("Address not provided").trim()
+            .isLength({ max: 70 }).withMessage("Address should be no more than 70 characters long"),
+        check("duns").exists({ checkFalsy: true }).withMessage("DUNS not provided").trim()
+            .isNumeric().withMessage("DUNS should contain only numeric values")
+            .isLength({ min: 9, max: 9 }).withMessage("DUNS should be 9 characters long (format: XXXXXXXXX)"),
+        check("zipcode").exists({ checkFalsy: true }).withMessage("Zip code not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("city").exists({ checkFalsy: true }).withMessage("City not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided")
     ], async (req, res) => {
         try {
             const errors = validationResult(req);
@@ -27,10 +43,9 @@ router.post("/createUser",
             const { typeOfUser, firstName, secondName, companyName, email, phone, address, duns, zipcode, city } = req.body;
             const user = new User(null, typeOfUser, firstName, secondName, companyName, email, phone, address, duns, zipcode, city);
             console.log(user)
-            const { userCreated, createdUser }   = await User.createUser(user)
+            const { userCreated, createdUser } = await User.createUser(user)
             if (userCreated) {
                 return res.status(200).json({ response: { createdUser } });
-        
             } else {
                 return res.status(500).json({ response: { message: "Internal Server Error" } });
             }
@@ -44,49 +59,68 @@ router.post("/createUser",
             });
         }
     })
-router.post("/updateUser", [
-    check("idCustomer", "Id of customer not provided").exists(),
-    check("typeOfUser", "Type of user not provided").exists(),
-    check("firstName", "First name not provided").exists(),
-    check("secondName", "Second name not provided").exists(),
-    check("companyName", "Company not provided").exists(),
-    check("email", "Email not provided").exists(),
-    check("phone", "Phone not provided").exists(),
-    check("address", "Address not provided").exists(),
-    check("duns", "Duns not provided").exists(),
-    check("zipcode", "Zip code not provided").exists(),
-    check("city", "City not provided").exists(),
-], async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-                message: "Invalid data while creating a user",
+router.post("/updateUser",
+    [
+        check("idCustomer").exists({ checkFalsy: true }).withMessage("Customer not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("typeOfUser").exists({ checkFalsy: true }).withMessage("Type of user not provided").trim().toInt()
+            .isInt({ min: 1, max: 2 }).withMessage("Wrong value provided"),
+        check("firstName").exists({ checkFalsy: true }).withMessage("First name not provided").trim()
+            .isAlpha().withMessage("First name should contain only characters")
+            .isLength({ max: 45 }).withMessage("First name should be no more than 45 characters long"),
+        check("secondName").exists({ checkFalsy: true }).withMessage("Second name not provided").trim()
+            .isAlpha().withMessage("Second name should contain only characters")
+            .isLength({ max: 45 }).withMessage("Second name should be no more than 45 characters long"),
+        check("companyName").exists().withMessage("Company not provided").trim()
+            .isAlphanumeric().withMessage("Company name can have only characters and/or numbers provided")
+            .isLength({ max: 45 }).withMessage("Company name should be no more than 45 characters long"),
+        check("email").exists({ checkFalsy: true }).withMessage("Email not provided").trim()
+            .normalizeEmail().isEmail().withMessage("Wrong email format")
+            .isLength({ max: 45 }).withMessage("Email should be no more than 45 characters long"),
+        check("phone").exists({ checkFalsy: true }).withMessage("Phone not provided").trim()
+            .isMobilePhone().withMessage("Wrong phone format")
+            .isLength({ max: 16 }).withMessage("Phone should be no more than 16 characters long"),
+        check("address").exists({ checkFalsy: true }).withMessage("Address not provided").trim()
+            .isLength({ max: 70 }).withMessage("Address should be no more than 70 characters long"),
+        check("duns").exists({ checkFalsy: true }).withMessage("DUNS not provided").trim()
+            .isNumeric().withMessage("DUNS should contain only numeric values")
+            .isLength({ min: 9, max: 9 }).withMessage("DUNS should be 9 characters long (format: XXXXXXXXX)"),
+        check("zipcode").exists({ checkFalsy: true }).withMessage("Zip code not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided"),
+        check("city").exists({ checkFalsy: true }).withMessage("City not provided").trim()
+            .toInt().isInt({ min: 0 }).withMessage("Wrong value provided")
+    ], async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Invalid data while creating a user",
+                });
+            }
+            const { idCustomer, typeOfUser, firstName, secondName, companyName, email, phone, address, duns, zipcode, city } = req.body;
+            const user = new User(idCustomer, typeOfUser, firstName, secondName, companyName, email, phone, address, duns, zipcode, city);
+            console.log(user);
+            const { userInfoIsSame, updatedUser } = await User.updateUser(user)
+            if (userInfoIsSame) {
+                return res.status(200).json({ response: user });
+            } else if (!userInfoIsSame && updatedUser === undefined) {
+                return res.status(500).json({ response: { message: "Internal Server Error" } });
+            } else if (userInfoIsSame) {
+                return res.status(400).json({ response: updatedUser, message: "User was not updated, because the user info is the same" });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: "Invalid data",
+                errors: [
+                    { value: error, msg: error.message },
+                ],
             });
         }
-        const { idCustomer, typeOfUser, firstName, secondName, companyName, email, phone, address, duns, zipcode, city } = req.body;
-        const user = new User(idCustomer, typeOfUser, firstName, secondName, companyName, email, phone, address, duns, zipcode, city);
-        console.log(user);
-        const { userInfoIsSame, updatedUser } = await User.updateUser(user)
-        if (!userInfoIsSame && typeof updatedUser === 'object') {
-            return res.status(200).json({ response: updatedUser });
-        } else if (!userInfoIsSame && updatedUser === undefined) {
-            return res.status(500).json({ response: { message: "Internal Server Error" } });
-        } else if (userInfoIsSame) {
-            return res.status(400).json({ response: updatedUser, message: "User was not updated, because the user info is the same" });
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: "Invalid data",
-            errors: [
-                { value: error, msg: error.message },
-            ],
-        });
-    }
 
-})
+    })
+
 router.post("/getUser", async (req, res) => {
 
     const user = await User.getUser(43);
@@ -114,7 +148,7 @@ router.get("/getUsers", async (req, res) => {
 //             message: "Invalid data while deleting a user",
 //           });
 //        }
-       
+
 //        var {id} = req.body
 //        const response = await User.deleteUser(id)
 //        return res.status(200).json({response})
